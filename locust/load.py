d915c09ca3bcb59a7c5de632470034d581e82c40
Courd Headman <cheadman@republicservices.com>
2023-06-16 16:02:40 -0700

    troubleshooting

--- locust/load.py
+++ locust/load.py
@@ -6,8 +6,7 @@ from locust import HttpUser, task, between
 min_wait = float(os.getenv('LOCUST_MIN_WAIT', '1'))
 max_wait = float(os.getenv('LOCUST_MAX_WAIT', '3'))
 req_type = os.getenv('REQ_TYPE', 'GET')
-req_body = os.getenv('REQ_BODY', '')
-query_params = os.getenv('QUERY_PARAMS', '')
+req_body = json.loads(os.getenv('REQ_BODY', ''))
 headers = json.loads(os.getenv('LOCUST_HEADERS', '{}'))
 
 class MyUser(HttpUser):
@@ -17,9 +16,9 @@ class MyUser(HttpUser):
     def my_task(self):
         # Check the request type and perform the corresponding request
         if req_type.upper() == 'GET':
-            self.client.get("/", params=query_params, headers=headers)
+            self.client.get("/", headers=headers)
         elif req_type.upper() == 'POST':
-            self.client.post("/", json=req_body, headers=headers)
+            self.client.post(url="/", json=req_body, headers=headers)
         elif req_type.upper() == 'PATCH':
             self.client.patch("/", json=req_body, headers=headers)
         elif req_type.upper() == 'PUT':
